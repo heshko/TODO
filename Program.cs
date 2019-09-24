e0232363a1df2f4611d95cb35b3455f216628142
Heshko <shooter.metall@gmail.com>
2019-09-24 09:38:53 +0200

    Add menu

--- Program.cs
+++ Program.cs
@@ -6,8 +6,33 @@ namespace ToDo
     {
         static void Main(string[] args)
         {
-          
-        
+
+            bool ShouldRun = true;
+            while (ShouldRun)
+            {
+                Console.WriteLine("1. Add Todo ");
+                Console.WriteLine("2 .List  Tado!");
+                Console.WriteLine("3. Exit");
+
+                ConsoleKeyInfo PressedKey = Console.ReadKey();
+                switch (PressedKey.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.Clear();
+
+                        break;
+                    case ConsoleKey.D2:
+                        Console.Clear();
+                        break;
+                    case ConsoleKey.D3:
+                        Console.Clear();
+                        ShouldRun = false;
+
+                        break;
+
+                }
+
+            }
         }
     }
 }
